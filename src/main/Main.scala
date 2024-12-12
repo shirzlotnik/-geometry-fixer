d06c76f03a660c6c18e5ec70eacf3d2f973a1224
shirzlotnik <shirzlotnik@gmail.com>
2024-12-12 19:50:19 +0200

    cleanup

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -7,29 +7,6 @@ import org.locationtech.jts.geom._
 
 
 object Main {
-//  def fixDoubleCoords(geometry: Geometry): Geometry = {
-//    val coordinates = geometry.getCoordinates
-//    val geometryFactory = new GeometryFactory()
-//
-//    val newCoords = (0 until coordinates.length - 1)
-//      .foldLeft[(Array[Coordinate], Double)](Array[Coordinate](), Double.NaN) {
-//        case ((accCoords, lastM), i) => {
-//          val currM = (coordinates(i+1).y - coordinates(i).y)/(coordinates(i+1).x-coordinates(i).x)
-//          if(i == 0) (accCoords ++ Array(coordinates(i), coordinates(i+1)), currM)
-//          else {
-//
-//            if(lastM == currM) {
-//              val newFixCoords = (accCoords.slice(0, accCoords.length - 1) :+ coordinates(i+1))
-//              (newFixCoords, currM)
-//            } else (accCoords :+ coordinates(i+1), currM)
-//          }
-//        }
-//      }._1
-//
-//    val newGeom = geometryFactory.createPolygon(newCoords)
-//    newGeom
-//  }
-
 
   def fixGeometry(geometry: Geometry): Option[Geometry] = {
     try {
